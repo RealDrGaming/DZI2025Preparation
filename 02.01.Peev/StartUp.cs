eed1710030f317d7857b1fb36ad1aaccbe64ec58
Yordan Neshev <dankoneshev@abv.bg>
2025-02-01 13:18:00 +0200

    working exercise - done

---
+++ 02.01.Peev/StartUp.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> list = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    list.Add(Console.ReadLine());
+}
+
+string wordToSearch = Console.ReadLine();
+string wordToSearchReverse = Reverse(wordToSearch);
+bool atLeastOneWordContains = false;
+
+foreach (string s in list)
+{
+	if (s.Replace(" ", "").Contains(wordToSearch) || s.Replace(" ", "").Contains(wordToSearchReverse))
+	{
+        Console.WriteLine(s);
+        atLeastOneWordContains = true;
+    }
+}
+
+if (!atLeastOneWordContains)
+    Console.WriteLine("no");
+
+string Reverse(string text)
+{
+    char[] array = text.ToCharArray();
+    Array.Reverse(array);
+    return new String(array);
+}
