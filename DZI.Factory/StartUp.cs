fdf3a0867e4944a529ddb814e54a8372dd5826de
RealDrGaming <114016316+RealDrGaming@users.noreply.github.com>
2025-03-26 14:14:44 +0200

    reshenie na zad

---
+++ DZI.Factory/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace DZI.Factory
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+
+            int n = int.Parse(await sr.ReadLineAsync());
+            List<Wood> matList = new List<Wood>();
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] line = (await sr.ReadLineAsync()).Split(" ");
+
+                string code = line[0];
+                string name = line[1];
+                double amountInCubic = double.Parse(line[2]);
+                int daysViable = int.Parse(line[3]);
+                char type = line[4][0];
+                string comeInDate = line[5];
+                string siloPosition = line[6];
+                int? neededHumidity = int.Parse(line[7]);
+
+                Wood wood = new Wood(code, name, amountInCubic, daysViable, type, comeInDate, siloPosition, neededHumidity);
+
+                matList.Add(wood);
+            }
+
+            sr.Close();
+
+            matList.OrderBy(x => x.SiloPosition);
+
+            foreach (Wood wood in matList)
+            {
+                Console.WriteLine(wood.ToString());
+            }
+
+            matList.OrderBy(x => x.ComeInDate).ThenByDescending(x => x.DaysViable);
+
+            foreach (Wood wood in matList)
+            {
+                if (wood.Type == 'E')
+                {
+                    Console.WriteLine(wood.ToString());
+                }
+            }
+
+
+        }
+    }
+}
---
+++ DZI.Factory/Wood.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace DZI.Factory
+{
+    public class Wood
+    {
+        public Wood(string code, string name, double amountInCubic, int daysViable, char type, string comeInDate, string siloPosition, int? neededHumidity)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.AmountInCubic = amountInCubic;
+            this.DaysViable = daysViable;
+            this.Type = type;
+            this.ComeInDate = comeInDate;
+            this.SiloPosition = siloPosition;
+            this.NeededHumidity = neededHumidity;
+        }
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double AmountInCubic { get; set; }
+        public int DaysViable { get; set; }
+        public char Type { get; set; }
+        public string ComeInDate { get; set; }
+        public string SiloPosition { get; set; }
+        public int? NeededHumidity { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{SiloPosition}, {Code}, {Name}, {AmountInCubic}, {ComeInDate}, {DaysViable}");
+
+            if (this.Type == 'E')
+                sb.Append($", B%={NeededHumidity:f2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
