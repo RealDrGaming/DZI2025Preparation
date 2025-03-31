a0792bda8d5d37e9a457e1332d334e81131690dd
RealDrGaming <114016316+RealDrGaming@users.noreply.github.com>
2025-03-31 11:22:36 +0300

    rand peev zdch

---
+++ 03.29.Peev/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _03._29.Peev
+{
+    internal class Program
+    {
+        static int ev(int n)
+        {
+            int count = 0;
+            while (n % 2 == 0)
+            {
+                n /= 2;
+                count++;
+            }
+            return count;
+        }
+
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int p = int.Parse(input[0]);
+            int q = int.Parse(input[1]);
+
+            int maxEv = -1;
+            int bestNumber = p;
+
+            for (int i = p; i <= q; i++)
+            {
+                int currentEv = ev(i);
+                if (currentEv > maxEv || (currentEv == maxEv && i > bestNumber))
+                {
+                    maxEv = currentEv;
+                    bestNumber = i;
+                }
+            }
+
+            Console.WriteLine(bestNumber);
+        }
+    }
+}
